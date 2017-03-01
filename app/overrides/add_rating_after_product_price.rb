f57e11862a961764af76db4c33f0f1afa9fa3e93
Lakhvir <lakhvirkumar@998.com>
2017-03-01 11:36:34 +0530

    Removed rating to product index

--- app/overrides/add_rating_after_product_price.rb
+++
@@ -1,6 +0,0 @@
-Deface::Override.new(
-    virtual_path: 'spree/shared/_products',
-    name: 'add_rating_after_inside_product_rating',
-    insert_bottom: '[data-hook="product_rating"]',
-    partial: 'spree/shared/rating'
-)
