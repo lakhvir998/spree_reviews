09e92d44763978114a7d9cefa7a6b0264daf9e49
Lakhvir <lakhvirkumar@998.com>
2017-03-01 16:21:14 +0530

    Removed foregn key

--- db/migrate/20170208060051_create_spree_reviews.rb
+++ db/migrate/20170208060051_create_spree_reviews.rb
@@ -2,7 +2,7 @@ class CreateSpreeReviews < ActiveRecord::Migration[5.0]
   def change
     create_table :spree_reviews do |t|
       t.belongs_to :product
-      t.belongs_to :user, foreign_key: true
+      t.belongs_to :user
       t.string :name
       t.integer :rating
       t.text :review
