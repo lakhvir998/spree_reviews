ba3eb3fa1377f95d23b2a384ead02ecfbb385fc3
Lakhvir <lakhvirkumar@998.com>
2017-03-01 11:29:11 +0530

    Added rating to product index

--- app/overrides/add_rating_after_product_price.rb
+++ app/overrides/add_rating_after_product_price.rb
@@ -1,5 +1,5 @@
 Deface::Override.new(
-    virtual_path: 'spree/products/index',
+    virtual_path: 'spree/shared/_product',
     name: 'add_rating_after_inside_product_rating',
     insert_bottom: '[data-hook="product_rating"]',
     partial: 'spree/shared/rating'
